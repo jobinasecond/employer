68615ef13d417cebe4c113961f0a824d772d7fc4
Scott Jobinasecond <scott@jobinasecond.com>
2013-03-21 12:16:46 +0000

    native alerts

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,13 @@
 var app = {
     initialize: function() {
         this.bind();
+        
+
+        this.store = new MemoryStore(function () {
+            self.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
     },
     bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
@@ -38,5 +45,17 @@ var app = {
         document.querySelector('#' + id + ' .pending').className += ' hide';
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
+    },
+    
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
     }
+},
+
+
+
+
 };
